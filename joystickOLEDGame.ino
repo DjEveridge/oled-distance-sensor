ae9e41331e11c18ce1681eba9f71db0268584908
DJ M Everidge Jr <david.everidge1436@students.eccc.edu>
2026-04-06 11:23:58 -0500

    Refactored code into modular functions, improved radar performance, and added non-blocking buzzer logic

--- joystickOLEDGame.ino
+++ joystickOLEDGame.ino
@@ -3,33 +3,102 @@
 #include <Servo.h>
 #include "SR04.h"
 
-
+//Joystick pins
 const byte xPin = A3;
 const byte yPin = A2;
 const byte swPin = 2;
+//Ultrasonic Sensor pins
 const byte trigPin = 4;
 const byte echoPin = 5;
-const byte buzzerPin = 12;
-int servoPos = 0;
-int servoDirection = 1;
-int buzzerDelay = 50;
-int maxDist = 0;
+//Ultrasonic Sensor readings: Max, Min, Current
+int maxDist;
 int minDist;
-int radarMagnitude;
-long uSensorDist;
-long input;
+int uSensorDist;
+//Servo variables
+const byte servoPin = 3;
+int servoPos = 0;
+//Buzzer variables
+const byte buzzerPin = 12;
 unsigned long previousTime = 0;
 bool buzzerState = false;
+byte buzzerDelay = 50;
+int radarMagnitude;
 
-
+//Instantiate compnent instances
 U8G2_SSD1309_128X64_NONAME0_1_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 Servo myServo;
 SR04 usSensor = SR04(echoPin, trigPin);
 
+//prototypes
+void drawRadar();
+void updateOLED();
+void updateServoPos();
+void updateBuzzer();
+
+
+void setup() {
+  int initialDist = usSensor.Distance();//Calibrate max,min distances
+  maxDist = initialDist;
+  minDist = initialDist;
+  u8g2.begin();//Start writing to OLED
+  pinMode(swPin, INPUT);//Setup joystick click pin
+  digitalWrite(swPin, HIGH);
+  Serial.begin(9600);
+  myServo.attach(servoPin);//Setup servo motor
+  myServo.write(0);
+  Wire.setClock(100000);  // force standard I2C speed
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  //Initiate dynamic variables
+  int xInput = analogRead(xPin);//Get Joystick x input
+  uSensorDist = usSensor.Distance();
+
+  //Update each component using functional programming
+  updateServoPos(xInput);
+  updateOLED();
+  updateBuzzer();
+
+}
+
+
+void updateOLED() {
+  char buffer[20];
+  sprintf(buffer, "%d", uSensorDist);
+
+  if(uSensorDist < minDist) {
+    minDist = uSensorDist;
+  }
+  if(uSensorDist > maxDist) {
+    maxDist = uSensorDist;
+  }
+
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB08_tr);
+
+    u8g2.drawStr(0, 20, buffer);
+    u8g2.drawStr(20, 20, "cm" );
+
+    u8g2.setFont(u8g2_font_5x8_tr);
+
+    char minDistBuffer[20];
+    sprintf(minDistBuffer, "Min: %dcm", minDist);
+    u8g2.drawStr(0, 60, minDistBuffer);
+
+    char maxDistBuffer[20];
+    sprintf(maxDistBuffer, "Max: %dcm", maxDist);
+    u8g2.drawStr(80, 60, maxDistBuffer);
+
+    drawRadar();
+
+  } while (u8g2.nextPage());
+}
 
 void drawRadar() {
-  for(int angle = 0; angle <= 180; angle += 1) {
-  float rad = angle * 3.14159265 / 180;
+  for(int angle = 0; angle <= 180; angle += 5) {
+  float rad = radians(angle);
   int x = 80 + 30*cos(rad);
   int y = 40 - 30 * sin(rad);
   u8g2.drawPixel(x,y);
@@ -40,38 +109,34 @@ void drawRadar() {
   } else {
     radarMagnitude = uSensorDist;
   }
+
   u8g2.drawLine(80, 40,
-    80 + 30*cos(servoPos * 3.14159265 / 180.0) * radarMagnitude / 100,
-    40 - 30 * sin(servoPos * 3.14159265 / 180.0) * radarMagnitude / 100
+    80 + 30*cos(radians(servoPos)) * radarMagnitude / 100,
+    40 - 30 * sin(radians(servoPos)) * radarMagnitude / 100
   );
 
-  int blipX = 80 + 30*cos(servoPos * 3.14159265 / 180.0) * radarMagnitude / 100;
-  int blipY = 40 - 30 * sin(servoPos * 3.14159265 / 180.0) * radarMagnitude / 100;
+  int blipX = 80 + 30*cos(radians(servoPos)) * radarMagnitude / 100;
+  int blipY = 40 - 30 * sin(radians(servoPos)) * radarMagnitude / 100;
   u8g2.drawCircle(blipX, blipY, 2);
 }
 
+void updateServoPos(int x) {
+  if(x > 600) servoPos-= 3;
+  if(x < 400) servoPos+= 3;
 
-void setup() {
-  u8g2.begin();
-  int initialDist = usSensor.Distance();
-  pinMode(swPin, INPUT);
-  digitalWrite(swPin, HIGH);
-  Serial.begin(9600);
-  myServo.attach(3);
-  myServo.write(0);
-  Wire.begin();
-  Wire.setClock(100000);  // force standard I2C speed
-  pinMode(buzzerPin, OUTPUT);
-  maxDist = initialDist;
-  minDist = initialDist;
+
+  if (servoPos > 180) {
+    servoPos = 180;
+  }
+  if (servoPos < 0) {
+    servoPos = 0;
+  }
+
+  myServo.write(servoPos);
 }
 
-void loop() {
-  int xInput = analogRead(xPin);
-  int yInput = analogRead(yPin);
-  uSensorDist = usSensor.Distance();
+void updateBuzzer() {
   unsigned long currentTime = millis();
-
   if(uSensorDist < 5) {
     buzzerDelay = 5;
   } else if (uSensorDist < 10) {
@@ -91,61 +156,4 @@ void loop() {
   } else {
     digitalWrite(buzzerPin, LOW);
   }
-
-
-
-
-  input = uSensorDist;
-
-  if(xInput > 600) {
-    servoPos -= 8;
-  }
-
-  if(xInput < 400) {
-    servoPos += 8;
-  }
-
-  myServo.write(servoPos);
-
-  if (servoPos > 180) {
-    servoPos = 180;
-  }
-  if (servoPos < 0) {
-    servoPos = 0;
-  }
-
-  char buffer[20];
-  sprintf(buffer, "%ld", input);
-
-  if(uSensorDist < minDist) {
-    minDist = uSensorDist;
-  }
-  if(uSensorDist > maxDist) {
-    maxDist = uSensorDist;
-  }
-
-  u8g2.firstPage();
-  do {
-    u8g2.setFont(u8g2_font_ncenB08_tr);
-
-
-
-    u8g2.drawStr(0, 20, buffer);
-    u8g2.drawStr(20, 20, "cm" );
-    u8g2.setFont(u8g2_font_5x8_tr);
-    char minDistBuffer[20];
-    sprintf(minDistBuffer, "Min: %dcm", minDist);
-    u8g2.drawStr(0, 60, minDistBuffer);
-    char maxDistBuffer[20];
-    sprintf(maxDistBuffer, "Max: %dcm", maxDist);
-    u8g2.drawStr(80, 60, maxDistBuffer);
-
-
-    drawRadar();
-
-
-
-  } while (u8g2.nextPage());
-
-  delay(2);  // 👈 slower = more accurate readings
 }
